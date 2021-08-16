734cf16089ce3e3cb596b172ede6a48f9ffc58cf
Juan Jaminton Yate <hamtino@gmail.com>
2021-08-16 00:27:36 -0500

    cambio en feature1

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log('Corriendo código desde mi primer script!!!');
 const express = require('express');
 //Muestro en consola al objeto express
 console.log(express);
+var oneLinerJoke = require('one-liner-joke');
+
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke.body);
