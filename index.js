cb84f39a1038171552fcf27ecfa69c67a3dbd14b
Juan Jaminton Yate <hamtino@gmail.com>
2021-08-16 00:30:44 -0500

    segundo cambio en feature2

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ logger.debug(`Hola Mundo de Node`);
 logger.info(`Información de último momento, Node.js es lo más!!`);
 logger.warn(`Tirando warnings como campeones`);
 logger.error(`Algo no está bien!!!`);
+const Logger = require('logplease');
+
+module.exports = function saludar(nombre, apellido, saludo = ()=>{
+    console.log('Luego de saludar se ejecuta el callback');
+}) {
+    console.log(`Hola ${nombre} ${apellido}`);
+    saludo();
+}
+
+let mostrar = require('./index.js');
+mostrar('Ezequiel', 'Romero');
